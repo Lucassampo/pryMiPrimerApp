79a9b2c91ce6ab41d78f407d94995861cef2fc94
root <mgiraudo@ies21.edu.ar>
2023-04-21 12:42:23 -0300

    210523

--- pryMiPrimerApp/Form1.Designer.cs
+++ pryMiPrimerApp/Form1.Designer.cs
@@ -28,19 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.lblSueldo = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.mrcGastos = new System.Windows.Forms.GroupBox();
-            this.lblAlquiler = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.lblmpuestos = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.lblGastosExtras = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.lblAhorro = new System.Windows.Forms.Label();
             this.textBox4 = new System.Windows.Forms.TextBox();
+            this.lblGastosExtras = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lblmpuestos = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblAlquiler = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.lblAhorro = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
             this.mrcGastos.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -78,40 +79,29 @@
             this.mrcGastos.TabStop = false;
             this.mrcGastos.Text = "GASTOS";
             // 
-            // lblAlquiler
+            // textBox5
             // 
-            this.lblAlquiler.AutoSize = true;
-            this.lblAlquiler.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAlquiler.Location = new System.Drawing.Point(6, 48);
-            this.lblAlquiler.Name = "lblAlquiler";
-            this.lblAlquiler.Size = new System.Drawing.Size(55, 17);
-            this.lblAlquiler.TabIndex = 0;
-            this.lblAlquiler.Text = "Alquiler";
-            this.lblAlquiler.Click += new System.EventHandler(this.label2_Click);
+            this.textBox5.Location = new System.Drawing.Point(80, 170);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(74, 20);
+            this.textBox5.TabIndex = 9;
             // 
-            // textBox2
+            // lblAhorro
             // 
-            this.textBox2.Location = new System.Drawing.Point(80, 45);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(74, 20);
-            this.textBox2.TabIndex = 3;
+            this.lblAhorro.AutoSize = true;
+            this.lblAhorro.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAhorro.Location = new System.Drawing.Point(6, 170);
+            this.lblAhorro.Name = "lblAhorro";
+            this.lblAhorro.Size = new System.Drawing.Size(58, 17);
+            this.lblAhorro.TabIndex = 8;
+            this.lblAhorro.Text = "Ahorros";
             // 
-            // lblmpuestos
+            // textBox4
             // 
-            this.lblmpuestos.AutoSize = true;
-            this.lblmpuestos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblmpuestos.Location = new System.Drawing.Point(6, 92);
-            this.lblmpuestos.Name = "lblmpuestos";
-            this.lblmpuestos.Size = new System.Drawing.Size(72, 17);
-            this.lblmpuestos.TabIndex = 4;
-            this.lblmpuestos.Text = "Impuestos";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(80, 91);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(74, 20);
-            this.textBox3.TabIndex = 5;
+            this.textBox4.Location = new System.Drawing.Point(80, 133);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(74, 20);
+            this.textBox4.TabIndex = 7;
             // 
             // lblGastosExtras
             // 
@@ -123,12 +113,40 @@
             this.lblGastosExtras.TabIndex = 6;
             this.lblGastosExtras.Text = "Gastos Extras";
             // 
-            // textBox4
+            // textBox3
             // 
-            this.textBox4.Location = new System.Drawing.Point(80, 133);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(74, 20);
-            this.textBox4.TabIndex = 7;
+            this.textBox3.Location = new System.Drawing.Point(80, 91);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(74, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // lblmpuestos
+            // 
+            this.lblmpuestos.AutoSize = true;
+            this.lblmpuestos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmpuestos.Location = new System.Drawing.Point(6, 92);
+            this.lblmpuestos.Name = "lblmpuestos";
+            this.lblmpuestos.Size = new System.Drawing.Size(72, 17);
+            this.lblmpuestos.TabIndex = 4;
+            this.lblmpuestos.Text = "Impuestos";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(80, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(74, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // lblAlquiler
+            // 
+            this.lblAlquiler.AutoSize = true;
+            this.lblAlquiler.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAlquiler.Location = new System.Drawing.Point(6, 48);
+            this.lblAlquiler.Name = "lblAlquiler";
+            this.lblAlquiler.Size = new System.Drawing.Size(55, 17);
+            this.lblAlquiler.TabIndex = 0;
+            this.lblAlquiler.Text = "Alquiler";
+            this.lblAlquiler.Click += new System.EventHandler(this.label2_Click);
             // 
             // button1
             // 
@@ -153,23 +171,6 @@
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // lblAhorro
-            // 
-            this.lblAhorro.AutoSize = true;
-            this.lblAhorro.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAhorro.Location = new System.Drawing.Point(6, 170);
-            this.lblAhorro.Name = "lblAhorro";
-            this.lblAhorro.Size = new System.Drawing.Size(58, 17);
-            this.lblAhorro.TabIndex = 8;
-            this.lblAhorro.Text = "Ahorros";
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(80, 170);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(74, 20);
-            this.textBox5.TabIndex = 9;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -180,6 +181,7 @@
             this.Controls.Add(this.mrcGastos);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.lblSueldo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Gestion de Gastos Mensuales";
             this.Load += new System.EventHandler(this.Form1_Load);
