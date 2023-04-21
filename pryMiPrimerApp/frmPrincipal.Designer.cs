60eb4226c7037d5fb291e062faea3d8ab6b98a7b
root <mgiraudo@ies21.edu.ar>
2023-04-21 14:23:46 -0300

    210423

--- pryMiPrimerApp/frmPrincipal.Designer.cs
+++ pryMiPrimerApp/frmPrincipal.Designer.cs
@@ -28,37 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.cmdAlacena = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 46);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(354, 48);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Alacena";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 127);
+            this.button2.BackgroundImage = global::pryMiPrimerApp.Properties.Resources._128439824_gettyimages_71418271;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(12, 187);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(354, 50);
+            this.button2.Size = new System.Drawing.Size(354, 105);
             this.button2.TabIndex = 1;
             this.button2.Text = "Gestion De Gastos";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // cmdAlacena
+            // 
+            this.cmdAlacena.BackgroundImage = global::pryMiPrimerApp.Properties.Resources._5a3d336a_3649_40b2_aed4_a64b1f1c6101_16_9_aspect_ratio_default_0;
+            this.cmdAlacena.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.cmdAlacena.Font = new System.Drawing.Font("Monotype Corsiva", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdAlacena.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.cmdAlacena.Location = new System.Drawing.Point(12, 46);
+            this.cmdAlacena.Name = "cmdAlacena";
+            this.cmdAlacena.Size = new System.Drawing.Size(354, 99);
+            this.cmdAlacena.TabIndex = 0;
+            this.cmdAlacena.Text = "Alacena";
+            this.cmdAlacena.UseVisualStyleBackColor = true;
+            this.cmdAlacena.Click += new System.EventHandler(this.button1_Click);
+            // 
             // frmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(378, 223);
+            this.ClientSize = new System.Drawing.Size(378, 352);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmdAlacena);
             this.Name = "frmPrincipal";
             this.Text = "frmPrincipal";
             this.ResumeLayout(false);
@@ -67,7 +74,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button cmdAlacena;
         private System.Windows.Forms.Button button2;
     }
 }
