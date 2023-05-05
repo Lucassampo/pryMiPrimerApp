013ec876b4a818efbd596c57e5d702725840c4e8
Laboratorios de Informática IES 21 <laboratoriosies@hotmail.com>
2023-05-05 14:02:19 -0300

    050523

--- pryMiPrimerApp/Form1.Designer.cs
+++ pryMiPrimerApp/Form1.Designer.cs
@@ -30,18 +30,20 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGestionDeGastos));
             this.lblSueldo = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtSueldo = new System.Windows.Forms.TextBox();
             this.mrcGastos = new System.Windows.Forms.GroupBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.txtAhorros = new System.Windows.Forms.TextBox();
             this.lblAhorro = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtGastosExtras = new System.Windows.Forms.TextBox();
             this.lblGastosExtras = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtImpuestos = new System.Windows.Forms.TextBox();
             this.lblmpuestos = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtAlquiler = new System.Windows.Forms.TextBox();
             this.lblAlquiler = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.cmdGuardar = new System.Windows.Forms.Button();
+            this.cmdCancelar = new System.Windows.Forms.Button();
+            this.txtSueldoSobrante = new System.Windows.Forms.TextBox();
+            this.lblSueldoSobrante = new System.Windows.Forms.Label();
             this.mrcGastos.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -55,36 +57,38 @@
             this.lblSueldo.TabIndex = 0;
             this.lblSueldo.Text = "SUELDO";
             // 
-            // textBox1
+            // txtSueldo
             // 
-            this.textBox1.Location = new System.Drawing.Point(146, 25);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(74, 20);
-            this.textBox1.TabIndex = 2;
+            this.txtSueldo.Location = new System.Drawing.Point(146, 25);
+            this.txtSueldo.Name = "txtSueldo";
+            this.txtSueldo.Size = new System.Drawing.Size(74, 20);
+            this.txtSueldo.TabIndex = 2;
             // 
             // mrcGastos
             // 
-            this.mrcGastos.Controls.Add(this.textBox5);
+            this.mrcGastos.Controls.Add(this.lblSueldoSobrante);
+            this.mrcGastos.Controls.Add(this.txtSueldoSobrante);
+            this.mrcGastos.Controls.Add(this.txtAhorros);
             this.mrcGastos.Controls.Add(this.lblAhorro);
-            this.mrcGastos.Controls.Add(this.textBox4);
+            this.mrcGastos.Controls.Add(this.txtGastosExtras);
             this.mrcGastos.Controls.Add(this.lblGastosExtras);
-            this.mrcGastos.Controls.Add(this.textBox3);
+            this.mrcGastos.Controls.Add(this.txtImpuestos);
             this.mrcGastos.Controls.Add(this.lblmpuestos);
-            this.mrcGastos.Controls.Add(this.textBox2);
+            this.mrcGastos.Controls.Add(this.txtAlquiler);
             this.mrcGastos.Controls.Add(this.lblAlquiler);
             this.mrcGastos.Location = new System.Drawing.Point(66, 48);
             this.mrcGastos.Name = "mrcGastos";
-            this.mrcGastos.Size = new System.Drawing.Size(191, 207);
+            this.mrcGastos.Size = new System.Drawing.Size(191, 229);
             this.mrcGastos.TabIndex = 3;
             this.mrcGastos.TabStop = false;
             this.mrcGastos.Text = "GASTOS";
             // 
-            // textBox5
+            // txtAhorros
             // 
-            this.textBox5.Location = new System.Drawing.Point(80, 170);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(74, 20);
-            this.textBox5.TabIndex = 9;
+            this.txtAhorros.Location = new System.Drawing.Point(80, 170);
+            this.txtAhorros.Name = "txtAhorros";
+            this.txtAhorros.Size = new System.Drawing.Size(74, 20);
+            this.txtAhorros.TabIndex = 9;
             // 
             // lblAhorro
             // 
@@ -96,12 +100,12 @@
             this.lblAhorro.TabIndex = 8;
             this.lblAhorro.Text = "Ahorros";
             // 
-            // textBox4
+            // txtGastosExtras
             // 
-            this.textBox4.Location = new System.Drawing.Point(80, 133);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(74, 20);
-            this.textBox4.TabIndex = 7;
+            this.txtGastosExtras.Location = new System.Drawing.Point(80, 133);
+            this.txtGastosExtras.Name = "txtGastosExtras";
+            this.txtGastosExtras.Size = new System.Drawing.Size(74, 20);
+            this.txtGastosExtras.TabIndex = 7;
             // 
             // lblGastosExtras
             // 
@@ -113,12 +117,12 @@
             this.lblGastosExtras.TabIndex = 6;
             this.lblGastosExtras.Text = "Gastos Extras";
             // 
-            // textBox3
+            // txtImpuestos
             // 
-            this.textBox3.Location = new System.Drawing.Point(80, 91);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(74, 20);
-            this.textBox3.TabIndex = 5;
+            this.txtImpuestos.Location = new System.Drawing.Point(80, 91);
+            this.txtImpuestos.Name = "txtImpuestos";
+            this.txtImpuestos.Size = new System.Drawing.Size(74, 20);
+            this.txtImpuestos.TabIndex = 5;
             // 
             // lblmpuestos
             // 
@@ -130,12 +134,12 @@
             this.lblmpuestos.TabIndex = 4;
             this.lblmpuestos.Text = "Impuestos";
             // 
-            // textBox2
+            // txtAlquiler
             // 
-            this.textBox2.Location = new System.Drawing.Point(80, 45);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(74, 20);
-            this.textBox2.TabIndex = 3;
+            this.txtAlquiler.Location = new System.Drawing.Point(80, 45);
+            this.txtAlquiler.Name = "txtAlquiler";
+            this.txtAlquiler.Size = new System.Drawing.Size(74, 20);
+            this.txtAlquiler.TabIndex = 3;
             // 
             // lblAlquiler
             // 
@@ -148,38 +152,54 @@
             this.lblAlquiler.Text = "Alquiler";
             this.lblAlquiler.Click += new System.EventHandler(this.label2_Click);
             // 
-            // button1
+            // cmdGuardar
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button1.Location = new System.Drawing.Point(66, 261);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(78, 37);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "GUARDAR";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.cmdGuardar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.cmdGuardar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.cmdGuardar.Location = new System.Drawing.Point(12, 283);
+            this.cmdGuardar.Name = "cmdGuardar";
+            this.cmdGuardar.Size = new System.Drawing.Size(78, 37);
+            this.cmdGuardar.TabIndex = 4;
+            this.cmdGuardar.Text = "GUARDAR";
+            this.cmdGuardar.UseVisualStyleBackColor = false;
+            this.cmdGuardar.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // cmdCancelar
             // 
-            this.button2.BackColor = System.Drawing.Color.Silver;
-            this.button2.Location = new System.Drawing.Point(158, 261);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 37);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "CANCELAR";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.cmdCancelar.BackColor = System.Drawing.Color.Silver;
+            this.cmdCancelar.Location = new System.Drawing.Point(233, 283);
+            this.cmdCancelar.Name = "cmdCancelar";
+            this.cmdCancelar.Size = new System.Drawing.Size(75, 37);
+            this.cmdCancelar.TabIndex = 5;
+            this.cmdCancelar.Text = "CANCELAR";
+            this.cmdCancelar.UseVisualStyleBackColor = false;
+            this.cmdCancelar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtSueldoSobrante
+            // 
+            this.txtSueldoSobrante.Location = new System.Drawing.Point(101, 199);
+            this.txtSueldoSobrante.Name = "txtSueldoSobrante";
+            this.txtSueldoSobrante.Size = new System.Drawing.Size(74, 20);
+            this.txtSueldoSobrante.TabIndex = 10;
+            // 
+            // lblSueldoSobrante
+            // 
+            this.lblSueldoSobrante.AutoSize = true;
+            this.lblSueldoSobrante.Location = new System.Drawing.Point(9, 202);
+            this.lblSueldoSobrante.Name = "lblSueldoSobrante";
+            this.lblSueldoSobrante.Size = new System.Drawing.Size(86, 13);
+            this.lblSueldoSobrante.TabIndex = 11;
+            this.lblSueldoSobrante.Text = "Sueldo Sobrante";
             // 
             // frmGestionDeGastos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(311, 332);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmdCancelar);
+            this.Controls.Add(this.cmdGuardar);
             this.Controls.Add(this.mrcGastos);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtSueldo);
             this.Controls.Add(this.lblSueldo);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmGestionDeGastos";
@@ -195,18 +215,20 @@
         #endregion
 
         private System.Windows.Forms.Label lblSueldo;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtSueldo;
         private System.Windows.Forms.GroupBox mrcGastos;
         private System.Windows.Forms.Label lblAlquiler;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtGastosExtras;
         private System.Windows.Forms.Label lblGastosExtras;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtImpuestos;
         private System.Windows.Forms.Label lblmpuestos;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox txtAlquiler;
+        private System.Windows.Forms.Button cmdGuardar;
+        private System.Windows.Forms.Button cmdCancelar;
+        private System.Windows.Forms.TextBox txtAhorros;
         private System.Windows.Forms.Label lblAhorro;
+        private System.Windows.Forms.Label lblSueldoSobrante;
+        private System.Windows.Forms.TextBox txtSueldoSobrante;
     }
 }
 
--- pryMiPrimerApp/Form1.cs
+++ pryMiPrimerApp/Form1.cs
@@ -12,6 +12,13 @@ namespace pryMiPrimerApp
 {
     public partial class frmGestionDeGastos : Form
     {
+        decimal varSueldo;
+        decimal varAlquieler;
+        decimal varImpuestos;
+        decimal varGastosExtras;
+        decimal varAhorros;
+        decimal varSueldoSobrante;
+
         public frmGestionDeGastos()
         {
             InitializeComponent();
@@ -24,12 +31,26 @@ namespace pryMiPrimerApp
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            varSueldo = 0;
+            varAlquieler = 0;
+            varImpuestos = 0;
+            varGastosExtras = 0;
+            varAhorros = 0;
+            
 
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {
-
+        {   
+            varSueldo=Convert.ToDecimal(txtSueldo.Text);
+            varAlquieler=Convert.ToDecimal(txtAlquiler.Text);
+            varImpuestos=Convert.ToDecimal(txtImpuestos.Text);
+            varGastosExtras=Convert.ToDecimal(txtGastosExtras.Text);
+            varAhorros = Convert.ToDecimal(txtAhorros.Text);
+        
+        varSueldoSobrante = varSueldo - (varAlquieler + varImpuestos + varGastosExtras + varAhorros );
+        
+            txtSueldoSobrante.Text = varSueldoSobrante.ToString();
         }
 
         private void button2_Click(object sender, EventArgs e)
